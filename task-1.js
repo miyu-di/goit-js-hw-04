6d64fd1f1e303e30d04e57d0f36b5ea97c1214ac
miyu-di <141654479+miyu-di@users.noreply.github.com>
2023-11-07 17:56:34 +0200

    done

---
+++ task-1.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function isEnoughCapacity(products, containerSize) {
+  let totalContSize = 0;
+  for (const key in products) {
+    totalContSize += products[key];
+  }
+
+  if (totalContSize <= containerSize) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+);
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+);
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+);
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+);
